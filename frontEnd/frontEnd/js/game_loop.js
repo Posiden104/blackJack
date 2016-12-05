f81808bb684ae8260d1138928765c7c75aec0e13
Wyatt Larkey <wlarkey@purdue.edu>
2016-12-04 20:18:07 -0500

    Add files via upload

--- frontEnd/frontEnd/js/game_loop.js
+++ frontEnd/frontEnd/js/game_loop.js
@@ -1,6 +1,8 @@
 
 var interval = setInterval(gameLoop, 250);
 
+var cardNum = ["1", "2", "3", "4", "5"];
+
 function gameLoop() {
     
     // call for JSON, get data
@@ -8,6 +10,13 @@ function gameLoop() {
     
     // update board (whose turn it is)
     // call write_p1 through p6, dealer, alert
+    
+    document.getElementById("p1").innerHTML = "New text!";
+    document.getElementById("p2").innerHTML = "New text!";
+    document.getElementById("p3").innerHTML = "New text!";
+    document.getElementById("p4").innerHTML = "New text!";
+    document.getElementById("p5").innerHTML = "New text!";
+    document.getElementById("p6").innerHTML = "New text!";
     
     //if (player must wait) {
         //wait
