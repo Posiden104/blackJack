913984e5abba0eae27af9a9c49546d26b8d66e6b
Wyatt Larkey <wlarkey@purdue.edu>
2016-12-04 20:41:06 -0500

    Add files via upload

--- frontEnd/frontEnd/js/write_p1.js
+++ frontEnd/frontEnd/js/write_p1.js
@@ -1,28 +1,29 @@
 
 var message = '';
-var playerName = "SomeGuy";
 
-if(player.playerID == 1) {
-	playerName = "Player 1";
+if(players[0] == null) {
+    
+    else {
+        message = 'No Data Available';
+        document.write(message);
+    }  
 }
+else {
+    
+    var playerName = "Player 1"
+    var playerHand = player.hand;
+    var cardNum = ["1", "2", "3", "4", "5"];
+    var handSize = playerHand.length;
 
-
-var playerHand = player.hand;
-var cardNum = ["1", "2", "3", "4", "5"];
-var handSize = playerHand.length;
-
-//Check if Google is in the watchlist
-if (1) {
     message = '<div class="panel-body">'  + playerName + '</div><div class="panel-score">' + player.Score + '</div><div class="panel-bet">' + player.bet + '</div>';
     
     for (i = 0; i < handSize; i++) {
         
-        message = message + '<img class="card' + cardNum[i] + '" src="pics/' + playerHand[i] + '.png" class="img-thumbnail" alt="Card1">';
+            message = message + '<img class="card' + cardNum[i] + '" src="pics/' + playerHand[i] + '.png" class="img-thumbnail" alt="Card1">';
     }
     
-        document.write(message);
-}
-else {
-    message = 'No Data Available';
     document.write(message);
-}
+      
+}	
+
+
